bad29255fb244b49330506e4fd38a5107a8fe14e
jkilfoy <jordankilfoy@gmail.com>
2022-02-09 22:41:56 -0500

    add controller; use config context

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useContext, useReducer } from 'react';
 import './App.css';
+import { Controller } from './components/Controller';
 import World from './components/World';
+import { ConfigContext } from './context/ConfigContext';
+
+let configReducer = (config, prop, newVal) => {
+    config[prop] = newVal;
+    return config;
+}
 
 function App() {
-  return (
-      <World size={16} />
-  );
+    // let config = useContext(ConfigContext);
+    const [config, dispatch] = useReducer(configReducer, {});
+    const configValues = {
+        config: config,
+        dispatch: dispatch
+    };
+
+    return (
+        <ConfigContext.Provider value={configValues}>
+            <Controller />
+            <World size={16} />
+        </ConfigContext.Provider>
+    );
 }
 
 export default App;
---
+++ src/components/Controller.js
@@ -0,0 +1,10 @@
+import { useContext, useReducer } from "react";
+import { WorldContext } from "../context/ConfigContext";
+
+
+export const Controller = (initialValues) => {
+    // todo
+    return (
+        <div>Hope hope hope hope</div>
+    )
+}
